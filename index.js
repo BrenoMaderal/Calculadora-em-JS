799fa62af86957a6f0471ba4f97bf22921dc584b
BrenoMaderal <brenomaderalortiz@gmail.com>
2026-01-20 18:13:30 -0300

    Commit Calculadora

---
+++ index.js
@@ -0,0 +1,68 @@
+
+function criaCalculadora() {
+    return{
+        display: document.querySelector('.display'),
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0,-1)
+        },
+        
+        fazConta(){
+            let conta = this.display.value
+
+            try{
+                conta = eval(conta)
+
+                if(!conta){
+                    alert('Conta Invalida')
+                    return
+                }
+                this.display.value = conta
+            } catch(e){
+                alert('Conta Invalida')
+                return
+            }
+        },
+
+        inicia: function(){
+            this.cliqueBotao()
+            this.pressionaEnter()
+        },
+
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13){
+                    this.fazConta()
+                }    
+            })
+        },
+
+        cliqueBotao(){
+            document.addEventListener('click', e =>{
+                const el = e.target
+                if(el.classList.contains('btn-num')){
+                   this.btnParaDisplay(el.innerText)
+                }
+                if(el.classList.contains('btn-clear')){
+                    this.display.value = ''
+                }
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm()
+                }
+                if(el.classList.contains('btn-eq')){
+                    this.fazConta()
+                }
+             
+            })
+        },
+
+         btnParaDisplay(valor){
+            this.display.value += valor
+            this.display.focus()
+         },
+
+    }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
